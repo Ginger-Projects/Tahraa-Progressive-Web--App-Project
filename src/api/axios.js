806fb4db0deeeb2ded704284e0763854404ba8f4
Nijas ma <nijazz1@gmail.com>
2025-12-12 15:55:08 +0530

    backend url added

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL:import.meta.env.VITE_API_BASE_URL,
+    baseURL:"https://api.yanmu.qa/api",
     headers:{"Content-Type":"application/json"},
 
 })
