919aff3acc6cca7f91bb26d0caece1c5d4d56045
Nijas ma <nijazz1@gmail.com>
2025-12-13 12:13:28 +0530

    icon updated

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL:"https://tahraa.preview.gingertechnologies.app/api",
+    baseURL:"https://api.yanmu.qa/api",
     headers:{"Content-Type":"application/json"},
 
 })
